88484a66f77d6cfaeee3114646dfcce0a17fa7f1
Gisélida Melo <giselidac@gmail.com>
2023-10-08 18:37:10 -0300

    ✨ feat: structure modal

--- src/pages/about.page.ts/about.page.ts
+++ src/pages/about.page.ts/about.page.ts
@@ -1,12 +1,33 @@
 import "./about.page.scss";
 export class AboutPage extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
-      <div>
-        <h1>Details Page</h1>
-        <p>Here are the details.</p>
-        <a href="#home">Go back to Home</a>
+    this.innerHTML = createInnerHTML();
+
+    function createInnerHTML() {
+      return /*html*/ `
+   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
     `;
+    }
   }
 }
